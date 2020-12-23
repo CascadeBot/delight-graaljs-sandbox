c183a1031e3808a4eb211cc3f276f8b0ba546e58
weeryan17 <theweeryan17@gmail.com>
2020-12-23 14:58:08 -0600

    Remove secure bindings code

--- src/main/java/delight/graaljssandbox/internal/GraalSandboxImpl.java
+++ src/main/java/delight/graaljssandbox/internal/GraalSandboxImpl.java
@@ -119,15 +119,15 @@ public class GraalSandboxImpl extends NashornSandboxImpl implements GraalSandbox
 	 */	
     @Override
 	protected Bindings secureBindings(Bindings bindings) {
-        if (bindings == null) return null;
+        /*if (bindings == null) return null;
         Set<String> toRemove = new HashSet<String>();
         if (bindings != cached) {
         	for (Map.Entry<String, Object> entry : bindings.entrySet()) {
         	  if (cached.putIfAbsent(entry.getKey(), entry.getValue()) != null) toRemove.add(entry.getKey());
         	}
         }
-        for (String key : toRemove) bindings.remove(key);
-        return cached;
+        for (String key : toRemove) bindings.remove(key);*/
+        return bindings;
     }
     
     /**
@@ -139,7 +139,7 @@ public class GraalSandboxImpl extends NashornSandboxImpl implements GraalSandbox
 	@Override
 	public Object eval(final String js, final ScriptContext scriptContext, final Bindings bindings)
 			throws ScriptCPUAbuseException, ScriptException {
-		Set<String> addedKeys = new HashSet<>();
+		/*Set<String> addedKeys = new HashSet<>();
 		if (scriptContext != null) {
 			Bindings engineBindings = scriptEngine.getBindings(ScriptContext.ENGINE_SCOPE);
 			Bindings contextBindings = scriptContext.getBindings(ScriptContext.ENGINE_SCOPE);
@@ -149,7 +149,7 @@ public class GraalSandboxImpl extends NashornSandboxImpl implements GraalSandbox
 					else addedKeys.add(key);
 				}			
 			}
-		}
+		}*/
 	    produceSecureBindings(); // We need this here for bindings
 		final JsSanitizer sanitizer = getSanitizer();
 		// see https://github.com/javadelight/delight-nashorn-sandbox/issues/73
@@ -164,12 +164,12 @@ public class GraalSandboxImpl extends NashornSandboxImpl implements GraalSandbox
 			securedJs = sanitizer.secureJs(js);
 		}
         final Bindings securedBindings = secureBindings(bindings);
-        if (bindings != null) addedKeys.addAll(bindings.keySet());
+        /*if (bindings != null) addedKeys.addAll(bindings.keySet());*/
         EvaluateOperation op = new EvaluateOperation(isStrict ? "'use strict';" + securedJs : securedJs, scriptContext, securedBindings);
         try {
         	return executeSandboxedOperation(op);
         } finally {
-        	for (String key: addedKeys) cached.remove(key);
+        	/*for (String key: addedKeys) cached.remove(key);*/
         }
 	}
 }
